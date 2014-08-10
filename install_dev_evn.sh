9391427b787a0ee21a19dd8701ce3c090e5be202
user@red <user@red>
2014-08-10 17:50:40 +0900

    add install_dev_env.sh

---
+++ install_dev_evn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## install primitive pkgs
+pkgs='
+vim
+openssh-server
+git
+language-pack-ja
+language-pack-ja-base
+'
+sudo apt-get install -y ${pkgs}
+
+## for language ja
+rm -rf Documents/ examples.desktop Music/ Pictures/ Public/ Templates/ Videos/
+update-locale LANG=ja_JP.UTF-8 LANGUAGE="jp_JP:ja"
+sudo update-locale LANG=ja_JP.UTF-8 LANGUAGE="jp_JP:ja"
+souce /etc/default/locale
+source /etc/default/locale
+echo $LANG
+
