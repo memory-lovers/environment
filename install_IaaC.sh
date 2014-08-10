de0706ded44b7983d41b76470676fca415e9226e
user@red <user@red>
2014-08-10 18:10:38 +0900

    install vagrant from website

--- install_IaaC.sh
+++ install_IaaC.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-## install apt pkgs
-pkgs='
-vagrant
-'
-sudo apt-get install -y ${pkgs}
+## install vagrant
+vagrant_deb='vagrant_1.6.3_x86_64.deb'
+wget https://dl.bintray.com/mitchellh/vagrant/${vagrant_deb}
+dpkg -i ${vagrant_deb}
+
+
